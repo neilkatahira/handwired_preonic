95af8fd9adf8ad5daa5c344a3e6c5269153117ab
Neil <neilkatahira@gmail.com>
2020-07-02 02:55:49 -0700

    keymap update

--- handwire_preonic/keymaps/kb/keymap.c
+++ handwire_preonic/keymaps/kb/keymap.c
@@ -12,7 +12,6 @@ enum preonic_keycodes {
     ADJUST,
 };
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         [_QWERTY] = KEYMAP(
             KC_GESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC  ,
